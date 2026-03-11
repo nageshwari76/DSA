dc08af5eada28849a245b5b1434733ce5b00566a
Dharavath Nageshwari <dharavathnageshwari64@gmail.com>
2026-03-11 17:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.83%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
+        
+        bits = n.bit_length()
+        mask = (1 << bits) - 1
+        return n ^ mask
+        
